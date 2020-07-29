02b9b9d1a59fae87f1707f653c6dcfb1636baa42
Olga <savina5565@gmail.com>
2020-07-29 11:38:06 +0300

    Add models for database tables

---
+++ models/Sportsbook.js
@@ -0,0 +1,54 @@
+const Model = require("./BaseModel");
+
+class Sportsbook extends Model {
+  // Table name is the only required property.
+  static get tableName() {
+    return "sportsbooks";
+  }
+
+  // Optional JSON schema. This is not the database schema! Nothing is generated
+  // based on this. This is only used for validation. Whenever a model instance
+  // is created it is checked against this schema. http://json-schema.org/.
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: [],
+
+      properties: {
+        id: { type: ["string", "null"] },
+        name: { type: ["string", "null"], minLength: 1, maxLength: 255 },
+        state: { type: ["string","null"] },
+        data_endpoint: { type: "string", format: "null" },
+        login_url: { type: ["string", "null"] },
+        state_id: { type: "string" },
+
+      },
+    };
+    
+  }
+  static get relationMappings() {
+    const Account = require("./Account");
+    const State = require("./State");
+    return {
+      accounts: {
+        relation: Model.HasManyRelation,
+        modelClass: Account,
+        join: {
+          from: "sportsbooks.id",
+          to: "accounts.sportsbook_id"
+        }
+      },
+        state: {
+          relation: Model.BelongsToOneRelation,
+          modelClass: State,
+          join: {
+            from: "sportsbooks.state_id",
+            to: "states.id"
+          }
+        }
+    };
+  }
+}
+  
+
+module.exports = Sportsbook;
